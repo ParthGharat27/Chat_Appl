ed2d7229d96e2ee282abd50e1154d389edf15574
Parth Gharat <parth.gharat2002@gmail.com>
2024-04-03 19:26:19 +0530

    created user and chat APIs

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const {accessChat,fetchChats,createGroupChat,renameGroup,addToGroup,removeFromGroup} = require("../controllers/chatControllers")
 const { protect } = require("../middlerware/authMiddleware")
 
 const router = express.Router();
 
  router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
-//  router.route("/group").post(protect, createGroupChats);
-//  router.route("/rename").put(protect, renameGroup);
-//  router.route("/groupremove").post(protect, removeFromGroup);
-//  router.route("/groupadd").post(protect, addToGroup);
+ router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+ router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
